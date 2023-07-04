bd672a9c4329ab08ae5db8a272fb8dbd17091be1
Zeeshan <zeeshanthedev@gmail.com>
2023-07-04 23:16:51 +0500

    added a title

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Zeeshan Khalid - Full Stack Web Developer</title>
+      </Head>
       <body className="bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
         <Main />
         <NextScript />
